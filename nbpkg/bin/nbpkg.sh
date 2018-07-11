708f4b5eadae28ffea8d97fcfbcc341ccde18e50
fukachan <fukachan>
2018-07-11 02:06:01 +0000

    define templates to use pkg_* and pkgin if could. verified that install, remove and update operations work well. upgrade operation is wrong.

--- nbpkg/bin/nbpkg.sh
+++ nbpkg/bin/nbpkg.sh
@@ -1,4 +1,29 @@
 #!/bin/sh
+#
+# Copyright (C) 2018 Ken'ichi Fukamachi
+#   All rights reserved. This program is free software; you can
+#   redistribute it and/or modify it under 2-Clause BSD License.
+#   https://opensource.org/licenses/BSD-2-Clause
+#
+# mailto: fukachan@fml.org
+#    web: http://www.fml.org/
+#
+# $FML$
+# $Revision$
+#        NAME: nbpkg.sh
+# DESCRIPTION: a reference implementation of the user utiilty for 
+#              the granular update for NetBSD base system.
+# CODINGSTYLE: POSIX compliant (checked by running "bash --posix" this script)
+#
+
+
+############################################################
+####################      FUNCTIONS     ####################
+############################################################
+
+usage () {
+    echo "USAGE: $0 [-h] ..."
+}
 
 netbsd_resolve_version () {
     /usr/bin/uname -r
@@ -16,45 +41,92 @@ netbsd_resolve_machine_and_arch () {
 
 }
 
-
-usage () {
-    echo "USAGE: $0 [-h] ..."
+do_pkgin () {
+    echo env PKG_REPOS=$REPOS_PATH pkgin $*
+         env PKG_REPOS=$REPOS_PATH pkgin $*
 }
 
+do_install () {
+    if [ -x /usr/pkg/bin/pkgin ];then
+	do_pkgin install $*
+    else
+	echo pkg_add $*
+	pkg_add $*
+    fi
+}
+
+do_remove () {
+    if [ -x /usr/pkg/bin/pkgin ];then
+	do_pkgin remove $*
+    else
+	pkg_del $*
+    fi
+}
+
+do_update () {
+    if [ -x /usr/pkg/bin/pkgin ];then
+	do_pkgin update
+    else
+	do_init
+	if [ -x /usr/pkg/bin/pkgin ];then
+	    do_pkgin update
+	fi
+    fi
+}
+
+do_upgrade () {
+    if [ -x /usr/pkg/bin/pkgin ];then
+	do_pkgin upgrade
+    else
+	for p in $*
+	do
+	    pkg_del $p
+	    pkg_add $p
+	done
+    fi
+}
+
+do_init () {
+    pkg_add pkgin
+}
+
+do_direct_pkgin () {
+    do_pkgin $*
+}
+
+############################################################
+####################        MAIN        ####################
+############################################################
+
+set -u
+
+PATH=/usr/sbin:/usr/bin:/sbin:/bin:/usr/pkg/sbin:/usr/pkg/bin
+export PATH
 
 #
-# CONFIG
+# variables
 #
 rel=$(netbsd_resolve_version)
 arch=$(netbsd_resolve_machine_and_arch)
 host=basepkg.netbsd.fml.org
-PKG_PATH=http://$host/pub/NetBSD/basepkg/$rel/$arch/
+PKG_PATH=http://$host/pub/NetBSD/basepkg/$rel/$arch
+REPOS_PATH=http://basepkg.netbsd.fml.org/pub/NetBSD/basepkg/$rel/$arch
 export PKG_PATH
-
-prog_add=/usr/sbin/pkg_add
-prog_del=/usr/sbin/pkg_delete
-pkgdir=/var/db/basepkg
-#
-# CONFIG END
-#
-
-
-#
-# MAIN
-#
+export REPOS_PATH
 
 # debug
-echo PKG_PATH=$PKG_PATH
-echo $prog_del -K $pkgdir -v games-games-catman
-     $prog_del -K $pkgdir -v games-games-catman
-echo $prog_add -K $pkgdir -v games-games-catman
-     $prog_add -K $pkgdir -v games-games-catman
+echo PKG_PATH   $PKG_PATH    
+echo REPOS_PATH $REPOS_PATH
 
-
+if [ $# -eq 0 ]; then usage; exit 1;fi
 case $1 in
-    update  )                      ;;
-    upgrade )                      ;;
     help | -h | \?  ) usage; exit 1;;
+    install )  shift; do_install       $* ;;
+    remove  )  shift; do_remove        $* ;;
+    update  )  shift; do_update        $* ;;
+    upgrade )  shift; do_upgrade       $* ;;
+    init    )  shift; do_init          $* ;;
+    [a-z]*  )         do_direct_pkgin  $* ;;
     *               ) usage; exit 1;;
 esac
 

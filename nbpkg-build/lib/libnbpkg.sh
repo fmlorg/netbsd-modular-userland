63d80fdc9116ec6f48f2c2b6e4cc08d545fb41cd
Ken'ichi Fukamachi <fukachan@fml.org>
2018-10-07 20:16:23 +0900

    separete queue* functions to a newly created "libqueue.sh".

--- nbpkg-build/lib/libnbpkg.sh
+++ nbpkg-build/lib/libnbpkg.sh
@@ -63,45 +63,6 @@ dir_clean () {
 
 
 #
-# queue
-#
-queue_add () {
-    local name=$1
-    local vers=$2
-    local type=$3
-    local arch=$4
-
-    mkdir -p $queue_dir/$name/$vers/$type/$arch
-    logit "queue_add: $*"
-}
-
-queue_del () {
-    local name=$1
-    local vers=$2
-    local type=$3
-    local arch=$4
-
-    if [ -d $queue_dir/$name/$vers/$type/$arch ];then
-	rmdir $queue_dir/$name/$vers/$type/$arch
-    fi
-    logit "queue_del: $*"
-}
-
-queue_find () {
-    local name=$1
-    local vers=$2
-    local type=$3
-    local arch=$4
-
-    if [ -d $queue_dir/$name/$vers/$type/$arch ];then
-	echo 1
-    else
-	echo 0
-    fi
-}
-
-
-#
 # nbpgk functions
 
 #

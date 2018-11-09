cfad525741249dc08d5b28f2bb94101acdbac9db
Ken'ichi Fukamachi <fukachan@fml.org>
2018-11-09 18:02:06 +0900

    implemented run_hook()

--- nbpkg-build/lib/libutil.sh
+++ nbpkg-build/lib/libutil.sh
@@ -30,3 +30,18 @@ random_number () {
 unixtime () {
     echo $(date +%s)
 }
+
+
+run_hook () {
+    local hook=$1
+
+    if [ -f $hook ];then
+	logit "run_hook: run $hook"
+	(
+	    . $hook
+	)
+        if [ $? != 0 ];then
+	    fatal "run_hook: failed: $hook"
+        fi
+    fi
+}

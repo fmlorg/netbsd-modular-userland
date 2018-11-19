a246292572b6809788913f15d9659f79d8d1f906
Ken'ichi Fukamachi <fukachan@fml.org>
2018-11-19 23:39:29 +0900

    $type -> $branch

--- nbpkg-build/lib/libqueue.sh
+++ nbpkg-build/lib/libqueue.sh
@@ -3,34 +3,34 @@
 #             
 
 queue_add () {
-    local name=$1
-    local arch=$2
-    local type=$3
-    local vers=$4
+    local   name=$1
+    local   arch=$2
+    local branch=$3
+    local   vers=$4
 
-    mkdir -p $queue_dir/$name/$vers/$type/$arch
+    mkdir -p $queue_dir/$name/$vers/$branch/$arch
     logit "queue_add: $*"
 }
 
 queue_del () {
-    local name=$1
-    local arch=$2
-    local type=$3
-    local vers=$4
+    local   name=$1
+    local   arch=$2
+    local branch=$3
+    local   vers=$4
     
-    if [ -d $queue_dir/$name/$vers/$type/$arch ];then
-	rmdir $queue_dir/$name/$vers/$type/$arch
+    if [ -d $queue_dir/$name/$vers/$branch/$arch ];then
+	rmdir $queue_dir/$name/$vers/$branch/$arch
     fi
     logit "queue_del: $*"
 }
 
 queue_find () {
-    local name=$1
-    local arch=$2
-    local type=$3
-    local vers=$4
+    local   name=$1
+    local   arch=$2
+    local branch=$3
+    local   vers=$4
     
-    if [ -d $queue_dir/$name/$vers/$type/$arch ];then
+    if [ -d $queue_dir/$name/$vers/$branch/$arch ];then
 	echo 1
     else
 	echo 0

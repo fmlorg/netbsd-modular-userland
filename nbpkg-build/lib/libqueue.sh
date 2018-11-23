75ac80b4a9c90f8ef01a110e9acb096518a92da3
Ken'ichi Fukamachi <fukachan@fml.org>
2018-11-23 13:30:51 +0900

    fixed to better varname: $vers -> $build_date

--- nbpkg-build/lib/libqueue.sh
+++ nbpkg-build/lib/libqueue.sh
@@ -3,34 +3,34 @@
 #             
 
 queue_add () {
-    local   name=$1
-    local   arch=$2
-    local branch=$3
-    local   vers=$4
+    local       name=$1
+    local       arch=$2
+    local     branch=$3
+    local build_date=$4
 
-    mkdir -p $queue_dir/$name/$vers/$branch/$arch
+    mkdir -p $queue_dir/$name/$build_date/$branch/$arch
     logit "queue_add: $*"
 }
 
 queue_del () {
-    local   name=$1
-    local   arch=$2
-    local branch=$3
-    local   vers=$4
+    local       name=$1
+    local       arch=$2
+    local     branch=$3
+    local build_date=$4
     
-    if [ -d $queue_dir/$name/$vers/$branch/$arch ];then
-	rmdir $queue_dir/$name/$vers/$branch/$arch
+    if [ -d $queue_dir/$name/$build_date/$branch/$arch ];then
+	rmdir $queue_dir/$name/$build_date/$branch/$arch
     fi
     logit "queue_del: $*"
 }
 
 queue_find () {
-    local   name=$1
-    local   arch=$2
-    local branch=$3
-    local   vers=$4
+    local       name=$1
+    local       arch=$2
+    local     branch=$3
+    local build_date=$4
     
-    if [ -d $queue_dir/$name/$vers/$branch/$arch ];then
+    if [ -d $queue_dir/$name/$build_date/$branch/$arch ];then
 	echo 1
     else
 	echo 0
